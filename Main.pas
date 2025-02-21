316bedba15c733cd1ee29cb4e43b0037b0947f83
luxidea <contact@luxidea.net>
2025-02-22 02:20:30 +0900

    no message

--- Main.pas
+++ Main.pas
@@ -3,19 +3,28 @@
 interface //#################################################################### ■
 
 uses
-  System.Classes,
-  FMX.Controls, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Types, FMX.Forms,
+  System.Classes, System.Generics.Collections,
+  FMX.Controls, FMX.Controls.Presentation, FMX.StdCtrls, FMX.Types, FMX.Forms, FMX.ListBox,
+  LUX.Complex,
+  LUX.Complex.Diff,
+  LUX.C2.Gamma.Lanczos.Diff,
+  LUX.C2.Gamma.Ooura.Diff,
   Viewer;
 
 type
   TForm1 = class(TForm)
     Viewer1: TViewerFrame;
+    Panel1: TPanel;
+    LabelA: TLabel;
+    ComboBoxA: TComboBox;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure ComboBoxAChange(Sender: TObject);
   private
     { private 宣言 }
   public
     { public 宣言 }
+    _Gammas :TList<TdDoubleCFunc>;
   end;
 
 var
@@ -25,14 +34,36 @@ implementation //###############################################################
 
 {$R *.fmx}
 
+//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-     /////
+     _Gammas := TList<TdDoubleCFunc>.Create;
+
+     _Gammas.Add( LUX.C2.Gamma.Lanczos.Diff.Gamma7  );  // 0
+     _Gammas.Add( LUX.C2.Gamma.Lanczos.Diff.Gamma9  );  // 1
+     _Gammas.Add( LUX.C2.Gamma.Lanczos.Diff.Gamma11 );  // 2
+     _Gammas.Add( LUX.C2.Gamma.Lanczos.Diff.Gamma15 );  // 3
+     _Gammas.Add( LUX.C2.Gamma.Ooura  .Diff.Gamma   );  // 4
+
+     Viewer1.Area  := TDoubleAreaC.Create( -5, -5, +5, +5 );
+     Viewer1.DivX  := 256;
+     Viewer1.DivY  := 256;
+     Viewer1.Scale := 2*Sqrt(Pi);
+
+     ComboBoxAChange( Sender );
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
-     /////
+     _Gammas.Free;
+end;
+
+//------------------------------------------------------------------------------
+
+procedure TForm1.ComboBoxAChange(Sender: TObject);
+begin
+     Viewer1.Func := _Gammas[ ComboBoxA.ItemIndex ];
 end;
 
 end. //######################################################################### ■
--- Viewer.pas
+++ Viewer.pas
@@ -6,9 +6,10 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Viewport3D,
   LUX.D4x4,
-  LUX.FMX.Graphics.D3,
-  LUX.D1.Gamma.FMX.D3,
-  LUX.D1.Gamma.C2.Diff;
+  LUX.Complex,
+  LUX.Complex.Diff,
+  LUX.Complex.FMX.D3,
+  LUX.FMX.Graphics.D3;
 
 type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$【 T Y P E 】
 
@@ -22,13 +23,30 @@ type //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
        _MouseP :TPointF;
        _Angle  :TPointF;
      protected
-       _World3D  :TF3DWorld;
-       _Camera3D :TF3DCamera;
-       _Light3D  :TF3DLight;
-       _Gamma3D  :TGamma3D;
+       _World3D   :TF3DWorld;
+       _Camera3D  :TF3DCamera;
+       _Light3D   :TF3DLight;
+       _Complex3D :TComplex3D;
+       ///// A C C E S S O R
+       function GetFunc :TdDoubleCFunc;
+       procedure SetFunc( const Func_:TdDoubleCFunc );
+       function GetArea :TDoubleAreaC;
+       procedure SetArea( const Area_:TDoubleAreaC );
+       function GetDivX :Integer;
+       procedure SetDivX( const DivX_:Integer );
+       function GetDivY :Integer;
+       procedure SetDivY( const DivY_:Integer );
+       function GetScale :Double;
+       procedure SetScale( const Scale_:Double );
      public
        constructor Create( Owner_:TComponent ); override;
        destructor Destroy; override;
+       ///// P R O P E R T Y
+       property Func  :TdDoubleCFunc read GetFunc  write SetFunc ;
+       property Area  :TDoubleAreaC  read GetArea  write SetArea ;
+       property DivX  :Integer       read GetDivX  write SetDivX ;
+       property DivY  :Integer       read GetDivY  write SetDivY ;
+       property Scale :Double        read GetScale write SetScale;
      end;
 
 implementation //############################################################### ■
@@ -41,6 +59,64 @@ uses System.Math;
 
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& protected
 
+//////////////////////////////////////////////////////////////// A C C E S S O R
+
+function TViewerFrame.GetFunc :TdDoubleCFunc;
+begin
+     Result := _Complex3D.Func;
+end;
+
+procedure TViewerFrame.SetFunc( const Func_:TdDoubleCFunc );
+begin
+     _Complex3D.Func := Func_;
+end;
+
+//------------------------------------------------------------------------------
+
+function TViewerFrame.GetArea :TDoubleAreaC;
+begin
+     Result := _Complex3D.Area;
+end;
+
+procedure TViewerFrame.SetArea( const Area_:TDoubleAreaC );
+begin
+     _Complex3D.Area := Area_;
+end;
+
+//------------------------------------------------------------------------------
+
+function TViewerFrame.GetDivX :Integer;
+begin
+     Result := _Complex3D.DivX;
+end;
+
+procedure TViewerFrame.SetDivX( const DivX_:Integer );
+begin
+     _Complex3D.DivX := DivX_;
+end;
+
+function TViewerFrame.GetDivY :Integer;
+begin
+     Result := _Complex3D.DivY;
+end;
+
+procedure TViewerFrame.SetDivY( const DivY_:Integer );
+begin
+     _Complex3D.DivY := DivY_;
+end;
+
+//------------------------------------------------------------------------------
+
+function TViewerFrame.GetScale :Double;
+begin
+     Result := _Complex3D.Scale;
+end;
+
+procedure TViewerFrame.SetScale( const Scale_:Double );
+begin
+     _Complex3D.Scale := Scale_;
+end;
+
 //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& public
 
 constructor TViewerFrame.Create( Owner_:TComponent );
@@ -50,20 +126,19 @@ begin
      _World3D := TF3DWorld.Create( Viewport3D1 );
 
      _Camera3D := TF3DCamera.Create( _World3D );
-     _Camera3D.Pose := TSingleM4.RotateX( DegToRad( 0 ) )
-                     * TSingleM4.Translate( 0, 0, 20 );
+     _Camera3D.Pose := TSingleM4.Translate( 0, 0, 20 );
 
      Viewport3D1.Camera := _Camera3D.Camera;
 
      _Light3D := TF3DLight.Create( _Camera3D );
      _Light3D.Pose := TSingleM4.RotateX( DegToRad( -45 ) );
 
-     _Gamma3D := TGamma3D.Create( _World3D );
-     _Gamma3D.Material.Ambient  := TAlphaColorF.Create( 0.2, 0.2, 0.2 ).ToAlphaColor;
-     _Gamma3D.Material.Diffuse  := TAlphaColorF.Create( 0.8, 0.8, 0.8 ).ToAlphaColor;
-     _Gamma3D.Material.Specular := TAlphaColors.Null;
-     _Gamma3D.Material.Texture.LoadFromFile( '../../_DATA/Texture 1024x1024.png' );
-     _Gamma3D.Pose := TSingleM4.Translate( 1/2, -2*Sqrt(Pi), 0 );
+     _Complex3D := TComplex3D.Create( _World3D );
+     _Complex3D.Material.Ambient  := TAlphaColorF.Create( 0.2, 0.2, 0.2 ).ToAlphaColor;
+     _Complex3D.Material.Diffuse  := TAlphaColorF.Create( 0.8, 0.8, 0.8 ).ToAlphaColor;
+     _Complex3D.Material.Specular := TAlphaColors.Null;
+     _Complex3D.Material.Texture.LoadFromFile( '../../_DATA/Texture 1024x1024.png' );
+     _Complex3D.Pose := TSingleM4.Translate( 1/2, -2*Sqrt(Pi), 0 );
 end;
 
 destructor TViewerFrame.Destroy;
